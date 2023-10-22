22a08aa77af03f7830a4f25b25820969cca271ad
EsterDahri <esterdahri@gmail.com>
2023-10-23 00:16:25 +0200

    add accessory cards

---
+++ Layout/partials/dogAccessory.php
@@ -0,0 +1,14 @@
+<h3>Accessory:</h3>
+<div class="row">
+    <div class="">
+        <?php foreach ($products as $product) : ?>
+            <?php if ($product->getCategories()->getPet() == 'dog') : ?>
+                <?php if ($product->type == 'Accessory') : ?>
+                    <div class="col-3">
+                        <?php include ROOT . DS . 'Layout' . DS . 'partials' . DS . 'accessoryCard.php'; ?>
+                    </div>
+                <?php endif; ?>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </div>
+</div>
