bb755f69ca0b6241379b044a9c8327329f9d152a
EsterDahri <esterdahri@gmail.com>
2023-10-22 20:42:03 +0200

    add class Bed

--- index.php
+++ index.php
@@ -90,6 +90,16 @@ class Food extends Products
 
 class Bed extends Products
 {
+    public $type = 'Bed';
+    public $materials;
+    public $colour;
+
+    function __construct($_name, $_price, $_image, Categories $Categories, $_materials, $_colour)
+    {
+        parent::__construct($_image, $_name, $_price, $Categories);
+        $this->materials = $_materials;
+        $this->colour = $_colour;
+    }
 };
 
 class Toy extends Products
