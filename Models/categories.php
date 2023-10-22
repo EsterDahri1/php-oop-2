044c91f7ebba5f4cefa6c6096762489edc316e83
EsterDahri <esterdahri@gmail.com>
2023-10-22 22:34:19 +0200

    add function that implemets pet icon

--- Models/categories.php
+++ Models/categories.php
@@ -19,4 +19,13 @@ class Categories
     {
         return $this->pet;
     }
+
+    function getAnimal()
+    {
+        if (strtolower($this->pet) == 'dog') {
+            return '<img width="30" src="./img/dog.png" alt="black outline of a dog face">';
+        } elseif (strtolower($this->pet) == 'cat') {
+            return '<img width="30" src="./img/cat.png" alt="black outline of a cat face">';
+        }
+    }
 };
