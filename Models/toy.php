051fb56ca9d80444fcb6896ff1422b08fc8b63df
EsterDahri <esterdahri@gmail.com>
2023-10-22 21:07:31 +0200

    add setters and getters on toy.php

--- Models/toy.php
+++ Models/toy.php
@@ -9,8 +9,40 @@ class Toy extends Products
     function __construct($_name, $_price, $_image, Categories $Categories, $_materials, $_colour, $_weight)
     {
         parent::__construct($_image, $_name, $_price, $Categories);
+        $this->setMaterials($_materials);
+        $this->setColour($_colour);
+        $this->setWeight($_weight);
+    }
+
+    #setters
+    function setMaterials($_materials)
+    {
         $this->materials = $_materials;
+    }
+
+    function setColour($_colour)
+    {
         $this->colour = $_colour;
+    }
+
+    function setWeight($_weight)
+    {
         $this->weight = $_weight;
+    }
+
+    #getters
+    function getMaterials()
+    {
+        return $this->materials;
+    }
+
+    function getColour()
+    {
+        return $this->colour;
+    }
+
+    function getWeight()
+    {
+        return $this->weight;
     }
 };
