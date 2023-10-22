1f4f7370c4ea82c57b42b5a59c239bdc75168e27
EsterDahri <esterdahri@gmail.com>
2023-10-23 00:22:48 +0200

    add two categories files php

---
+++ Layout/partials/catItems.php
@@ -0,0 +1,8 @@
+<h2>Cat items</h2>
+
+<?php
+include ROOT . DS . 'Layout' . DS . 'partials' . 'catFood.php';
+include ROOT . DS . 'Layout' . DS . 'partials' . 'catBed.php';
+include ROOT . DS . 'Layout' . DS . 'partials' . 'catToy.php';
+include ROOT . DS . 'Layout' . DS . 'partials' . 'catAccessory.php';
+?>
---
+++ Layout/partials/dogItems.php
@@ -0,0 +1,8 @@
+<h2>Dog items</h2>
+
+<?php
+include ROOT . DS . 'Layout' . DS . 'partials' . 'dogFood.php';
+include ROOT . DS . 'Layout' . DS . 'partials' . 'dogBed.php';
+include ROOT . DS . 'Layout' . DS . 'partials' . 'dogToy.php';
+include ROOT . DS . 'Layout' . DS . 'partials' . 'dogAccessory.php';
+?>
