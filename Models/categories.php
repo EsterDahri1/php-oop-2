8a5a91a3dc4c234864897681ecca4a7536eda513
EsterDahri <esterdahri@gmail.com>
2023-10-22 22:08:55 +0200

    add functions on categories

--- Models/categories.php
+++ Models/categories.php
@@ -1,4 +1,22 @@
 <?php
 class Categories
 {
+    protected $pet;
+
+    function __construct($_pet)
+    {
+        $this->setPet($_pet);
+    }
+
+    #setter
+    function setPet($_pet)
+    {
+        $this->pet = $_pet;
+    }
+
+    #getter
+    function getPet()
+    {
+        return $this->pet;
+    }
 };
