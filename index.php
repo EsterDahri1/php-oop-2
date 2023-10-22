ed6868303ce7bce0aa10eebcbbf066e3a3eb1fc6
EsterDahri <esterdahri@gmail.com>
2023-10-22 21:00:58 +0200

    add HTML markup

--- index.php
+++ index.php
@@ -8,7 +8,29 @@ CONSEGNA:
 2) Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
 
 3) Bonus (non opzionale):
-organizzate il progetto come visto stamattina a lezione usando varie sottocartelle per inserire classi, layout e dati.
+organizzate il progetto come visto stamattina a lezione usando varie sottocartelle per inserire classi, layout e dati. ✔️
 -->
 
 <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boolpet</title>
+</head>
+
+<body>
+    <header>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+    </footer>
+</body>
+
+</html>
